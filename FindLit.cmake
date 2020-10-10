35ca8b3e4712ca2774dd5588c434e83c5a56ad1f
Egor Dorichev <egordorichev@gmail.com>
2020-10-10 11:22:41 +0300

    Minor fixes to the findlit cmake

--- FindLit.cmake
+++ FindLit.cmake
@@ -2,12 +2,8 @@ find_package(PkgConfig)
 pkg_check_modules(PC_LIT QUIET lit)
 set(LIT_DEFINITIONS ${PC_LIT_CFLAGS_OTHER})
 
-find_path(LIT_INCLUDE_DIR lit/lit.h
-	HINTS ${PC_LIT_INCLUDEDIR} ${PC_LIT_INCLUDE_DIRS}
-	PATH_SUFFIXES libxml2 )
-
-find_library(LIT_LIBRARY NAMES lit liblit
-	HINTS ${PC_LIT_LIBDIR} ${PC_LIT_LIBRARY_DIRS} )
+find_path(LIT_INCLUDE_DIR lit/lit.h HINTS ${PC_LIT_INCLUDEDIR} ${PC_LIT_INCLUDE_DIRS} PATH_SUFFIXES liblit)
+find_library(LIT_LIBRARY NAMES lit liblit HINTS ${PC_LIT_LIBDIR} ${PC_LIT_LIBRARY_DIRS})
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Lit  DEFAULT_MSG LIT_LIBRARY LIT_INCLUDE_DIR)
