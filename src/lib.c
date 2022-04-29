d90f16207e6fd9bdf8f157d2d0ab99914e77c4b6
eugodr <egordorichev@gmail.com>
2022-04-29 12:39:28 +0300

    Added embed-file example

--- src/lib.c
+++ src/lib.c
@@ -6,6 +6,9 @@ LIT_NATIVE(test) {
 	return NUMBER_VALUE(10);
 }
 
+extern const char lit_lib[];
+
 void lib_open_library(LitState* state) {
 	lit_define_native(state, "test", test_native);
+	lit_interpret(state, "lib", (char*) lit_lib);
 }
--- src/test.c
+++ src/test.c
@@ -3,7 +3,10 @@
 int main(int argc, const char** argv) {
 	LitState* state = lit_new_state();
 
+	lit_open_libraries(state);
 	lib_open_library(state);
 	lit_interpret_file(state, "test.lit");
+	lit_event_loop(state);
+
 	lit_free_state(state);
 }
